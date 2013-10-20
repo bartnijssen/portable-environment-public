c17579ae314c18501f8df1693081478f6f371c1a
Joe Hamman <jhamman@hydro.washington.edu>
2013-10-20 10:43:37 -0700

    source host dirs

--- setup/templates/init/initialize.bash
+++ setup/templates/init/initialize.bash
@@ -5,5 +5,6 @@ source $HOME/.conf
 source $CONF/common/etc/bashrc
 source $CONF/site/$SITE/etc/bashrc
 source $CONF/plat/$PLAT/etc/bashrc
+source $CONF/site/$SITE/host/$HOST/etc/bashrc
 
 # source project bashrc files also
